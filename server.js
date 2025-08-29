8bb417e59bb513488b09c00cf0f4b41a30b976fd
tera <peteramajor@MacBookPro.lan>
2025-08-29 19:55:48 -0400

    final

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import OpenAI from "openai";
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const OpenAI = require("openai");
 
 const app = express();
 app.use(cors());
